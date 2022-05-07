36777f6f5769d8267bbd22be4ef3009540db10c6
tankorsmash <tankorsmash@gmail.com>
2022-05-07 14:41:48 -0400

    Preparing for v6

---
+++ src/Evergreen/V6/Types.elm
@@ -0,0 +1,41 @@
+module Evergreen.V6.Types exposing (..)
+
+import Browser
+import Browser.Navigation
+import Lamdera
+import Url
+
+
+type alias FrontendModel =
+    { key : Browser.Navigation.Key
+    , message : String
+    , clicksFromBackend : Int
+    }
+
+
+type alias BackendModel =
+    { message : String
+    , clicks : Int
+    }
+
+
+type FrontendMsg
+    = UrlClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | NoOpFrontendMsg
+    | SendClickToBackend
+
+
+type ToBackend
+    = NoOpToBackend
+    | ToBackendClick
+
+
+type BackendMsg
+    = NoOpBackendMsg
+    | TestCC Lamdera.SessionId Lamdera.ClientId
+
+
+type ToFrontend
+    = NoOpToFrontend
+    | NewTotalClicks Int
