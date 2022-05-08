e217963559d36ecf66ac428b6f0c622791f13af5
tankorsmash <tankorsmash@gmail.com>
2022-05-07 23:55:21 -0400

    Preparing for v14

---
+++ src/Evergreen/Migrate/V14.elm
@@ -0,0 +1,78 @@
+module Evergreen.Migrate.V14 exposing (..)
+
+import Evergreen.V13.Types as Old
+import Evergreen.V14.Types as New
+import Lamdera.Migrations exposing (..)
+
+
+frontendModel : Old.FrontendModel -> ModelMigration New.FrontendModel New.FrontendMsg
+frontendModel old =
+    let
+        newModel : New.FrontendModel
+        newModel =
+            { key = old.key
+            , message = old.message
+            , totalClicksFromBackend = old.totalClicksFromBackend
+            , personalityTypeClicksFromBackend = old.personalityTypeClicksFromBackend
+            , userClicksFromBackend = old.userClicksFromBackend
+            , username = old.username
+            , user = convertUser old.user
+            , totalUsers = old.totalUsers
+            , usernamesByPersonalityTypes = old.usernamesByPersonalityTypes
+            }
+    in
+    ModelMigrated
+        ( newModel
+        , Cmd.none
+        )
+
+
+backendModel : Old.BackendModel -> ModelMigration New.BackendModel New.BackendMsg
+backendModel old =
+    ModelUnchanged
+
+
+frontendMsg : Old.FrontendMsg -> MsgMigration New.FrontendMsg New.FrontendMsg
+frontendMsg old =
+    MsgUnchanged
+
+
+toBackend : Old.ToBackend -> MsgMigration New.ToBackend New.BackendMsg
+toBackend old =
+    MsgUnchanged
+
+
+backendMsg : Old.BackendMsg -> MsgMigration New.BackendMsg New.BackendMsg
+backendMsg old =
+    MsgUnchanged
+
+
+toFrontend : Old.ToFrontend -> MsgMigration New.ToFrontend New.FrontendMsg
+toFrontend old =
+    MsgUnchanged
+
+convertPersonalityType : Old.PersonalityType -> New.PersonalityType
+convertPersonalityType old =
+    case old of
+        Old.Idealistic -> New.Idealistic
+        Old.Realistic -> New.Realistic
+
+convertUser : Old.User -> New.User
+convertUser old =
+    case old of
+        Old.AnonymousUser mbPt ->
+            New.AnonymousUser (Maybe.map (convertPersonalityType ) mbPt)
+
+        Old.PreppingUser sessionId pt ->
+            New.PreppingUser sessionId (convertPersonalityType pt)
+
+        Old.FullUser userData ->
+            New.FullUser (convertUserData userData)
+
+
+convertUserData old =
+    { personalityType = convertPersonalityType old.personalityType
+    , sessionId = old.sessionId
+    , username = old.username
+    , userClicks = old.userClicks
+    }
