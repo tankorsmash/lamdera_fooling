3c608361cab3d7791461f61b3acf43cbad5788cd
tankorsmash <tankorsmash@gmail.com>
2022-06-16 18:03:37 -0400

    add missing AdminPage.elm

---
+++ src/AdminPage.elm
@@ -0,0 +1,28 @@
+module AdminPage exposing (..)
+
+import Element exposing (Color, Element, alignBottom, alignLeft, alignRight, alignTop, centerX, centerY, column, el, explain, fill, fillPortion, height, modular, padding, paddingXY, paragraph, rgb, rgb255, row, scrollbars, spacing, spacingXY, text, width)
+import Types exposing (..)
+
+
+type alias Model =
+    AdminFrontendModel
+
+
+type alias Msg =
+    AdminFrontendMsg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    let
+        noop =
+            ( model, Cmd.none )
+    in
+    case msg of
+        NoOpAdminFrontend ->
+            noop
+
+
+view : Element Msg
+view =
+    text "New Admin Page"
